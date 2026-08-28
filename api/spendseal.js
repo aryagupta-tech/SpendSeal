d368e9810dea47c99cf361fef7b733a79f5b5120
Arya Gupta <aaryagupta.vns@gmail.com>
2026-08-28 17:39:50 +0530

    Fix SpendSeal Vercel API routing

---
+++ api/spendseal.js
@@ -0,0 +1,12 @@
+import handler from "../.vercel-build/index.js";
+
+export default async function vercelRoute(req, res) {
+  const url = new URL(req.url ?? "/", "http://spendseal.internal");
+  const publicPath = url.searchParams.get("__spendseal_path");
+  if (publicPath) {
+    url.searchParams.delete("__spendseal_path");
+    const query = url.searchParams.toString();
+    req.url = `${publicPath}${query ? `?${query}` : ""}`;
+  }
+  await handler(req, res);
+}
